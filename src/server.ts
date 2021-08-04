c7116738b0f4239a64a830641d880adfac011eb9
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-08-04 11:58:41 -0300

    User.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
+import "reflect-metadata";
 import express from "express";
-// @types/express
+
+import "./database";
+
 const app = express();
 
-app.get("/test", (request, response) => {
-    // request => entrando
-    // response => saindo
-    return response.send("Olá");
-});
+app.listen(3000, () => console.log("A mãe tá on"));
 
-app.post("/test-post", (request, response) => {
-    return response.send("Olá, método POST");
-});
-
-app.listen(3000 , () => console.log("Sever is running"));
