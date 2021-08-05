61755b11cedd3348fcaa11076b8b28937c74b1eb
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-08-05 11:58:47 -0300

    routes.ts

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,3 +1,6 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
 interface IUserRequest {
     name: string;
     email: string;
@@ -7,8 +10,30 @@ interface IUserRequest {
 class CreateUserService {
 
     async execute({ name, email, admin }: IUserRequest) {
+        const usersRepository = getCustomRepository(UsersRepositories);
 
+        if(!email) {
+            throw new Error("Email incorrect");  
+        }
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        });
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin,
+        });
+
+        await usersRepository.save(user);
+
+        return user;
     }
 }
 
-export { CreateUserService }
+export { CreateUserService };
