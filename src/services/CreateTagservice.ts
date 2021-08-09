f7fe7928c93099533cb5da3750020667266a527d
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-08-09 15:18:03 -0300

    ensureAdmin.ts

--- src/services/CreateTagservice.ts
+++ src/services/CreateTagservice.ts
@@ -11,9 +11,21 @@ class CreateTagService {
         }
 
         const tagAlreadyExists = await tagsRepositories.findOne({
+            name,
+        });
+
+        if (tagAlreadyExists) {
+            throw new Error("Tag already exists!");
+        }
+
+        const tag = tagsRepositories.create({
             name
-        })
+        });
+
+        await tagsRepositories.save(tag);
+
+        return tag;
     }
 }
 
-export { CreateTagService }
+export { CreateTagService };
