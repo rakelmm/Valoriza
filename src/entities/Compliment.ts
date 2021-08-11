7c767619a99979cbf7373d9e378d0c36bd93f98f
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-08-11 15:58:37 -0300

    Compliment.ts

---
+++ src/entities/Compliment.ts
@@ -0,0 +1,43 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, JoinColumn, ManyToOne } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { Tag } from "./Tag";
+import { User } from "./User";
+
+@Entity("compliments")
+class Compliment {
+
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    user_sender: string;
+
+    @JoinColumn({name: "user_sender"})
+    @ManyToOne(() => User)
+    userSender: User
+
+    @Column()
+    user_receiver: string;
+
+    @JoinColumn({name: "user_receiver"})
+    @ManyToOne(() => User)
+    userReceiver: User
+
+    @Column()
+    tag_id: string;
+
+    @JoinColumn({name: "tag_id"})
+    @ManyToOne(() => Tag)
+    tag: Tag;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Compliment };
